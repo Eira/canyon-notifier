0618eae2319a630ebb1e6848400296be8adc2de7
Irina <eira@mail.ru>
2022-11-23 21:38:52 +0100

    test get new availables bikes fixed

--- tests/tests_storage/test_get_available_bike_list.py
+++ tests/tests_storage/test_get_available_bike_list.py
@@ -1,9 +1,13 @@
+from app.models import Bike
 from app.storage import get_available_bike_list
 
 
 async def test_get_available_bike_list_happy_path(fixture_prefilled_available_bike_list):
     res = await get_available_bike_list()
 
-    assert res == fixture_prefilled_available_bike_list
+    assert isinstance(res, list)
+    for item in res:
+        assert isinstance(item, Bike)
+    assert set([bike.id for bike in res]) == set([bike.id for bike in fixture_prefilled_available_bike_list])
 
 # todo test на пустой каталог
