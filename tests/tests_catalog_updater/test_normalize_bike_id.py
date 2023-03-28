7a0ca73f8d4f390eb2895e58ca82fc47d6aec445
Irina <eira@mail.ru>
2023-03-28 08:57:53 +0200

    [review] refactoring bike id

--- tests/tests_catalog_updater/test_normalize_bike_id.py
+++ tests/tests_catalog_updater/test_normalize_bike_id.py
@@ -3,6 +3,17 @@ import pytest
 from app.catalog.catalog_operations import _normalize_bike_id
 
 
-@pytest.mark.parametrize("test_input,expected", [("Spectral 125 CF 9", "spectral_125_cf_9"), ("", "")])
-def test_normalize_bike_id(test_input, expected):
-    assert _normalize_bike_id(test_input) == expected
+def test_normalize_bike_id_happy_path():
+    test_input = 'Spectral 125 CF 9'
+    size = 'M'
+    expected = 'spectral_125_cf_9_m'
+
+    assert _normalize_bike_id(test_input, size) == expected
+
+
+def test_normalize_bike_id_no_data():
+    test_input = ''
+    size = ''
+
+    with pytest.raises(RuntimeError):
+        _normalize_bike_id(test_input, size)
